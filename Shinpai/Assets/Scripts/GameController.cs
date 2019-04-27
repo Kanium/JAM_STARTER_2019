c1d7eeaffa0734cf9c1908919d1fb5c6c331e365
haeejuut <haeejuut@student.jyu.fi>
2019-04-27 17:40:18 +0300

    mekaniikat bumpseja vailla tehtyna

---
+++ Shinpai/Assets/Scripts/GameController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class GameController : MonoBehaviour
+{
+    public Level current_level { get; set; }
+    public int hex_counter = 1;
+    public float time_between_hex;
+
+    public PlayerScript player;
+    public Text text_points;
+    public int points = 0;
+    public int bump_index = 0;
+    public Image[] bumps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        new_level();
+        var temp = GameObject.Find("bumps");
+        bumps = temp.GetComponentsInChildren<Image>();
+    }
+
+    /// <summary>
+    /// Initiate a new level
+    /// </summary>
+    void new_level()
+    {
+        new WaitForSecondsRealtime(5f);
+        Debug.Log("new level :" + hex_counter.ToString());
+        StopAllCoroutines();
+        hex_counter++;
+        current_level = new Level(hex_counter);
+        StartCoroutine(pump_hex(time_between_hex));
+
+        if (hex_counter % 2 == 0)
+        {
+            // make player rotate
+            // player.rotation_speed += 1.5f;
+        }
+
+        if (hex_counter % 3 == 0)
+        {
+            // upgrade hex properties
+            time_between_hex -= .15f;
+            current_level.rotator.fall_speed += .5f;
+            current_level.rotator._min += 5;
+            current_level.rotator._max += 15;
+        }
+    }
+
+    // pump a new hex out every t seconds
+    IEnumerator pump_hex(float t)
+    {   
+        yield return new WaitForSeconds(t);
+        Instantiate(current_level._jono.Dequeue());
+        // check for end of queue, keep pumping until all instantiated
+        if (current_level._jono.Count != 0)
+            StartCoroutine(pump_hex(t));
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check for end of queue
+        if(current_level._jono.Count == 0)
+            new_level();
+        text_points.text = points.ToString() + " " + bump_index.ToString();
+
+        if (bump_index == 5)
+            SceneManager.LoadScene("gameScene");
+    }
+
+    public class Level
+    {
+        public GameObject hex_prefab { get; set; }
+        public Queue<GameObject> _jono;
+        public Rotator rotator;
+
+        public Level(int number_of_hexes)
+        {
+            hex_prefab = Resources.Load("Prefabs/hex") as GameObject;
+            rotator = hex_prefab.GetComponent<Rotator>();
+
+            _jono = new Queue<GameObject>(number_of_hexes);
+            for (int i = 0; i < number_of_hexes; i++)
+            {
+                _jono.Enqueue(hex_prefab);
+            }
+        }
+    }
+
+}
---
+++ Shinpai/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float rotation_speed, poke_speed;
+    private Vector3 alotus, loppu;
+    private bool colliding = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        poke_speed = .2f;
+        alotus = transform.localScale;
+        loppu = new Vector3(transform.localScale.x,
+                            transform.localScale.y + 1.5f,
+                            transform.localScale.z);
+        current_state = State.IDLE;
+    }
+
+    State current_state;
+    enum State
+    {
+        IDLE,
+        POKING,
+        RETRACTING
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, -rotation_speed * Time.deltaTime));
+        bool keydown = Input.GetKeyDown(KeyCode.Space);
+        bool keyup = Input.GetKeyUp(KeyCode.Space);
+        bool keyhold = Input.GetKey(KeyCode.Space);
+
+        switch (current_state)
+        {
+            case State.IDLE:
+                if (keydown)
+                    current_state = State.POKING;
+                break;
+            case State.POKING:
+                if (keyup)
+                    current_state = State.RETRACTING;
+                transform.localScale += new Vector3(0, poke_speed, 0);
+                break;
+            case State.RETRACTING:
+                if (transform.localScale == alotus || transform.localScale.y <= alotus.y)
+                    current_state = State.IDLE;
+                transform.localScale += new Vector3(0, -poke_speed / 2, 0);
+                break;
+            default:
+                break;
+        }
+
+        // https://gist.github.com/sdabet/3bda94676a4674e6e4a0
+        // Handle native touch events
+        foreach (Touch touch in Input.touches)
+        {
+            HandleTouch(touch.fingerId, Camera.main.ScreenToWorldPoint(touch.position), touch.phase);
+        }
+
+        // Simulate touch events from mouse events
+        if (Input.touchCount == 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                HandleTouch(10, Camera.main.ScreenToWorldPoint(Input.mousePosition), TouchPhase.Began);
+            }
+            if (Input.GetMouseButton(0))
+            {
+                HandleTouch(10, Camera.main.ScreenToWorldPoint(Input.mousePosition), TouchPhase.Moved);
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                HandleTouch(10, Camera.main.ScreenToWorldPoint(Input.mousePosition), TouchPhase.Ended);
+            }
+        }
+    }
+
+    /// <summary>
+    /// touch events
+    /// </summary>
+    /// <param name="touchFingerId"></param>
+    /// <param name="touchPosition"></param>
+    /// <param name="touchPhase"></param>
+    private void HandleTouch(int touchFingerId, Vector3 touchPosition, TouchPhase touchPhase)
+    {
+        switch (touchPhase)
+        {
+            case TouchPhase.Began:
+                current_state = State.POKING;
+                break;
+            case TouchPhase.Moved:
+                // TODO
+                break;
+            case TouchPhase.Ended:
+                current_state = State.RETRACTING;
+                break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (colliding)
+        {
+            if (col.tag == "gameOverCollider")
+            {
+                Debug.Log("palkkiin osui");
+                col.GetComponentInParent<Rotator>().Drop();
+                colliding = false;
+
+            }
+            if (col.tag == "pointCollider")
+            {
+                Debug.Log("pisteeseen osui");
+                col.GetComponentInParent<Rotator>().Cleared();
+                colliding = false;
+            }
+
+        }
+    }
+}
